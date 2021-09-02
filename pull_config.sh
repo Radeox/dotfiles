7f71a42ebd25c05843180203654245c326e43123
Radeox <dawid.weglarz95@gmail.com>
2021-09-02 19:12:35 +0200

    Add lazy script

---
+++ pull_config.sh
@@ -0,0 +1,5 @@
+# Pull my configuration (cuz I'm lazy)
+cp ~/.config/alacritty.yml alacritty/alacritty.yml
+cp ~/.config/compton.conf compton/compton.conf
+cp ~/.config/i3/config i3/config
+cp ~/.my-aliases ~/.my-config ~/.zshrc zsh/
