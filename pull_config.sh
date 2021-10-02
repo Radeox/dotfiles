82add022745024cabb90d1138a364881e2c4710e
Radeox <dawid.weglarz95@gmail.com>
2021-10-03 00:27:41 +0200

    Updated picom configuration

--- pull_config.sh
+++ pull_config.sh
@@ -1,6 +1,6 @@
 # Pull my configuration (cuz I'm lazy)
 cp ~/.config/alacritty.yml alacritty/alacritty.yml
-cp ~/.config/compton.conf compton/compton.conf
+cp ~/.config/picom.conf picom/picom.conf
 cp ~/.config/i3/config i3/config
 cp ~/.my-aliases ~/.my-config ~/.zshrc zsh/
 cp ~/.vimrc vim/
