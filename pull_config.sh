95321d5c1cb50dd54c7f6f2e0362ee955237f6e0
Radeox <dawid.weglarz95@gmail.com>
2021-10-29 02:15:14 +0200

    Moved to polybar and rofi

--- pull_config.sh
+++ pull_config.sh
@@ -2,5 +2,7 @@
 cp ~/.config/alacritty.yml alacritty/alacritty.yml
 cp ~/.config/picom.conf picom/picom.conf
 cp ~/.config/i3/config i3/config
+cp -r ~/.config/rofi rofi
+cp -r ~/.config/polybar polybar
 cp ~/.my-aliases ~/.my-zshrc ~/.zshrc zsh/
 cp ~/.vimrc vim/
