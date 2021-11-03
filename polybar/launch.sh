360b133041786314961958bde44233be0f0e1502
Radeox <dawid.weglarz95@gmail.com>
2021-11-03 02:19:26 +0100

    Workaround polybar CPU temperature

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -8,5 +8,5 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 # Launch bar on each display
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar -q --reload main -c "$HOME/.config/polybar/config.ini" & disown
+    MONITOR=$m polybar -q main -c "$HOME/.config/polybar/config.ini" & disown
 done
