da76d7e8e86cd8f18a41f417ff3fcb46fa67254d
Dawid Weglarz <dawid.weglarz95@gmail.com>
2022-02-20 19:20:09 +0100

    Archived old config

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -1,8 +1,4 @@
 # Pull my configuration (cuz I'm lazy)
 cp ~/.config/kitty/kitty.conf kitty/kitty.conf
-cp ~/.config/picom.conf picom/picom.conf
-cp ~/.config/i3/config i3/config
-cp -r ~/.config/rofi .
-cp -r ~/.config/polybar/*.ini polybar/
 cp ~/.my-aliases ~/.my-zshrc ~/.zshrc zsh/
 cp ~/.vimrc vim/
