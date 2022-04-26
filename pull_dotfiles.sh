01eb864e7603bb00d6dba9f1c6464df45b0e7ffa
Dawid Weglarz <dawid.weglarz95@gmail.com>
2022-04-26 21:47:07 +0200

    Add vifm config

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -2,4 +2,5 @@
 cp ~/.config/kitty/kitty.conf kitty/kitty.conf
 cp ~/.my-aliases ~/.my-zshrc ~/.zshrc zsh/
 cp ~/.config/nvim/init.vim nvim/
+rm -r vifm && cp -r ~/.config/vifm . && rm vifm/vifminfo.json
 rm -r rofi && cp -r ~/.config/rofi .
