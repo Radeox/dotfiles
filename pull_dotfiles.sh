a8c6fa4605ee8dfee3eb93a3f21419d4748fb934
Dawid Weglarz <dawid.weglarz95@gmail.com>
2022-05-28 18:32:04 +0200

    Updated ZSH config

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -1,5 +1,5 @@
 # Pull my configuration (cuz I'm lazy)
 cp ~/.config/kitty/kitty.conf kitty/kitty.conf
-cp ~/.my-aliases ~/.my-zshrc ~/.zshrc zsh/
+cp ~/.aliases ~/.zshrc zsh/
 cp ~/.config/nvim/init.vim nvim/
 rm -r vifm && cp -r ~/.config/vifm . && rm vifm/vifminfo.json
