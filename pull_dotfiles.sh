421285a2afc15ccb24c361d178ea0010abf012c4
Dawid Weglarz <dawid.weglarz95@gmail.com>
2022-11-10 22:35:56 +0100

    Updated some stuff

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -2,6 +2,7 @@
 cp ~/.config/kitty/kitty.conf kitty/kitty.conf
 cp ~/.aliases ~/.zshrc zsh/
 cp ~/.config/nvim/*.vim nvim/
-cp -r ~/.config/waybar/ waybar/
-cp -r ~/.config/hypr/ hypr/
+cp -r ~/.config/waybar/ .
+cp -r ~/.config/hypr/ .
+cp -r ~/.config/wlogout/ .
 rm -r vifm && cp -r ~/.config/vifm . && rm vifm/vifminfo.json
