2fa5da47aefd853e6a0208fa370905e5923a23aa
Dawid Weglarz <dawid.weglarz95@gmail.com>
2022-12-11 23:51:22 +0100

    Archived Hyprland config

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -2,8 +2,4 @@
 cp ~/.config/kitty/kitty.conf kitty/kitty.conf
 cp ~/.aliases ~/.zshrc zsh/
 cp ~/.config/nvim/*.vim nvim/
-cp -r ~/.config/waybar/ .
-cp -r ~/.config/hypr/ .
-cp -r ~/.config/wlogout/ .
-cp -r ~/.config/kanshi/ .
 rm -r vifm && cp -r ~/.config/vifm . && rm vifm/vifminfo.json
