7df89d21d856aab70bfca61f13656c7056e725e9
Dawid Weglarz <dawid.weglarz95@gmail.com>
2023-08-02 21:41:39 +0200

    Splitted home-manager config by program

---
+++ nixos/home.nix.d/zsh.nix
@@ -0,0 +1,81 @@
+{ pkgs, ... }: {
+  # ZSH configuration
+  programs.zsh = {
+    enable = true;
+
+    enableAutosuggestions = true;
+    enableCompletion = true;
+    defaultKeymap = "viins";
+
+    shellAliases = {
+      # NixOS commands
+      nix-clean = "sudo nix-collect-garbage -d";
+      nix-config = "cd /etc/nixos && vim configuration.nix";
+      nix-update = "sudo nixos-rebuild switch";
+
+      # Aliases
+      ".." = "cd .. && ls";
+      f = "vifm";
+      find = "fd";
+      ll = "ls -l";
+      p = "ps aux | grep ";
+      pr = "poetry run python";
+      rgrep = "rg";
+      sl = "ls";
+      sudo = "sudo ";
+      tig = "lazygit";
+      v = "vim";
+      vim = "nvim";
+
+      # Basic commands
+      cat = "bat -p";
+      df = "duf";
+      du = "dust";
+      ls = "lsd";
+
+      # Scripts
+      bt_manager = "bash ~/Sources/scripts/bluetooth_manager.sh";
+      myip = "bash ~/Sources/scripts/my_ip.sh";
+      yee = "python ~/Sources/Projects/TermYeeLight/manager.py";
+    };
+
+    # Oh My ZSH Configuration
+    oh-my-zsh = {
+      enable = true;
+
+      plugins = [
+        "autojump"
+        "docker"
+        "docker-compose"
+        "pip"
+        "poetry"
+        "python"
+        "sudo"
+        "vscode"
+      ];
+    };
+
+    plugins = with pkgs; [{
+      name = "zsh-syntax-highlighting";
+      src = fetchFromGitHub {
+        owner = "zsh-users";
+        repo = "zsh-syntax-highlighting";
+        rev = "0.7.1";
+        sha256 = "0zmq66dzasmr5pwribyh4kbkk23jxbpdw4rjxx0i7dx8jjp2lzl4";
+      };
+      file = "zsh-syntax-highlighting.zsh";
+    }];
+
+    history = {
+      expireDuplicatesFirst = true;
+      save = 10000;
+      size = 10000;
+    };
+  };
+
+  # Starship configuration
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+}
