eccca456d992e7b38dcb705966f090e05c3e7ccc
Dawid Weglarz <dawid.weglarz95@gmail.com>
2023-12-11 17:10:34 +0100

    Updated catpuccin alacritty theme

--- nixos/home.nix.d/alacritty.nix
+++ nixos/home.nix.d/alacritty.nix
@@ -44,98 +44,100 @@
         startup_mode = "Maximized";
       };
 
-      # Colors (Catpuccin Macchiato - Green)
+      # Colors (Catpuccin Macchiato)
       colors = {
         primary = {
-          background = "#1E1E2E";
-          foreground = "#CDD6F4";
-
-          dim_foreground = "#CDD6F4";
-          bright_foreground = "#CDD6F4";
+          background = "#24273A";
+          foreground = "#CAD3F5";
+          dim_foreground = "#CAD3F5";
+          bright_foreground = "#CAD3F5";
         };
 
         cursor = {
-          text = "#1E1E2E";
-          cursor = "#F5E0DC";
+          text = "#24273A";
+          cursor = "#F4DBD6";
         };
 
         vi_mode_cursor = {
-          text = "#1E1E2E";
-          cursor = "#B4BEFE";
+          text = "#24273A";
+          cursor = "#B7BDF8";
         };
 
         search = {
           matches = {
-            foreground = "#1E1E2E";
-            background = "#A6ADC8";
+            foreground = "#24273A";
+            background = "#A5ADCB";
           };
+
           focused_match = {
-            foreground = "#1E1E2E";
-            background = "#A6E3A1";
+            foreground = "#24273A";
+            background = "#A6DA95";
           };
+
           footer_bar = {
-            foreground = "#1E1E2E";
-            background = "#A6ADC8";
+            foreground = "#24273A";
+            background = "#A5ADCB";
           };
         };
 
         hints = {
           start = {
-            foreground = "#1E1E2E";
-            background = "#F9E2AF";
+            foreground = "#24273A";
+            background = "#EED49F";
           };
           end = {
-            foreground = "#1E1E2E";
-            background = "#A6ADC8";
+            foreground = "#24273A";
+            background = "#A5ADCB";
           };
         };
 
         selection = {
-          text = "#1E1E2E";
-          background = "#F5E0DC";
+          text = "#24273A";
+          background = "#F4DBD6";
         };
 
         normal = {
-          black = "#45475A";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#BAC2DE";
+          black = "#494D64";
+          red = "#ED8796";
+          green = "#A6DA95";
+          yellow = "#EED49F";
+          blue = "#8AADF4";
+          magenta = "#F5BDE6";
+          cyan = "#8BD5CA";
+          white = "#B8C0E0";
         };
 
         bright = {
-          black = "#585B70";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#A6ADC8";
+          black = "#5B6078";
+          red = "#ED8796";
+          green = "#A6DA95";
+          yellow = "#EED49F";
+          blue = "#8AADF4";
+          magenta = "#F5BDE6";
+          cyan = "#8BD5CA";
+          white = "#A5ADCB";
         };
 
         dim = {
-          black = "#45475A";
-          red = "#F38BA8";
-          green = "#A6E3A1";
-          yellow = "#F9E2AF";
-          blue = "#89B4FA";
-          magenta = "#F5C2E7";
-          cyan = "#94E2D5";
-          white = "#BAC2DE";
+          black = "#494D64";
+          red = "#ED8796";
+          green = "#A6DA95";
+          yellow = "#EED49F";
+          blue = "#8AADF4";
+          magenta = "#F5BDE6";
+          cyan = "#8BD5CA";
+          white = "#B8C0E0";
         };
 
         indexed_colors = [
           {
             index = 16;
-            color = "#FAB387";
+            color = "#F5A97F";
           }
+
           {
             index = 17;
-            color = "#F5E0DC";
+            color = "#F4DBD6";
           }
         ];
       };
