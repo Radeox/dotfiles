83de225e3dd7af20c005c322f9cced24b1b59c5e
Dawid Weglarz <dawid.weglarz95@gmail.com>
2024-04-02 10:17:37 +0200

    Updated pull script

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -4,7 +4,7 @@
 cp -r /etc/nixos .
 
 ## AstroVim ##
-rm -r astrovim && mkdir astrovim && cp -r ~/.config/nvim/lua/user astrovim
+rm -r astrovim && mkdir astrovim && cp -r ~/.config/nvim/ astrovim
 
 ## Vifm ##
 rm -r vifm && cp -r ~/.config/vifm . && rm vifm/vifminfo.json
