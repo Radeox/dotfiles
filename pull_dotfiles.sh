24af0fea576da1d335a8bfc4bdd3621dbdf53c9a
Dawid Weglarz <dawid.weglarz95@gmail.com>
2024-05-10 18:11:32 +0200

    Updated pull script

--- pull_dotfiles.sh
+++ pull_dotfiles.sh
@@ -1,7 +1,10 @@
-# My dotfiles #
+# Pull dotfiles from system #
 
 ## NixOS ##
-cp -r /etc/nixos .
+rm -r nixos && cp -r /etc/nixos .
 
 ## AstroVim ##
 rm -r astrovim && mkdir astrovim && cp -r ~/.config/nvim/ astrovim
+
+## Hyprlock ##
+rm -r hyprlock && mkdir hyprlock && cp ~/.config/hypr/hyprlock.conf hyprlock/
