ad41235d65ec53cc30566112998f2252e05fa3f9
Dawid Weglarz <dawid.weglarz95@gmail.com>
2024-06-11 11:33:32 +0200

    Update copilot plugin in nvim

--- astrovim/nvim/lua/plugins/user.lua
+++ astrovim/nvim/lua/plugins/user.lua
@@ -6,22 +6,7 @@ return {
   { import = "astrocommunity.colorscheme.catppuccin" },
   
   -- Copilot
-  { import = "astrocommunity.completion.copilot-lua" },
-  { -- further customize the options set by the community
-    "zbirenbaum/copilot.lua",
-    opts = {
-      suggestion = {
-        keymap = {
-          accept = "<C-l>",
-          accept_word = false,
-          accept_line = false,
-          next = "<C-.>",
-          prev = "<C-,>",
-          dismiss = "<C/>",
-        },
-      },
-    },
-  },
+  { import = "astrocommunity.completion.copilot-cmp" },
 
   -- Override splash
   {
