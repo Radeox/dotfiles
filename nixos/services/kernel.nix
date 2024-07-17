2ffaacf4d6ec68ab3947876a52889b3eb87e6e75
Dawid Weglarz <dawid.weglarz95@gmail.com>
2024-07-17 21:31:14 +0200

    Revert to stock kernel

--- nixos/services/kernel.nix
+++ nixos/services/kernel.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   boot = {
     # Use latest xanmod kernel
-    kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    kernelPackages = pkgs.linuxPackages_latest;
 
     # NTFS support
     supportedFilesystems = [ "ntfs" ];
