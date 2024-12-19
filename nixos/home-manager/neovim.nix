e1c846dd9e17cb5aef73bc934d636f7643e93fa3
Dawid Weglarz <dawid.weglarz95@gmail.com>
2024-12-19 18:05:35 +0100

    Add neovim config in home manager

--- nixos/home-manager/neovim.nix
+++ nixos/home-manager/neovim.nix
@@ -2,7 +2,6 @@
   programs.neovim = {
     enable = true;
     defaultEditor = true;
-    catppuccin.enable = false;
 
     # Enable aliases
     viAlias = true;
@@ -11,5 +10,25 @@
     # Enable providers
     withNodeJs = true;
     withPython3 = true;
+
+    # Setup init.lua
+    extraLuaConfig = ''
+      local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+      if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+        -- stylua: ignore
+        vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+      end
+      vim.opt.rtp:prepend(lazypath)
+
+      if not pcall(require, "lazy") then
+        -- stylua: ignore
+        vim.api.nvim_echo({ { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } }, true, {})
+        vim.fn.getchar()
+        vim.cmd.quit()
+      end
+
+      require "lazy_setup"
+      require "polish"
+    '';
   };
 }
