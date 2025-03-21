d37b89bf49d4ba04de4540b4777460af2ee932fe
Dawid Weglarz <dawid.weglarz95@gmail.com>
2025-03-21 14:15:03 +0100

    Testing xanmod kernel

--- nixos/services/kernel.nix
+++ nixos/services/kernel.nix
@@ -1,7 +1,10 @@
 { pkgs, ... }: {
   boot = {
     # Use latest stock kernel
-    kernelPackages = pkgs.linuxPackages_latest;
+    # kernelPackages = pkgs.linuxPackages_latest;
+
+    # Use latest xanmod kernel
+    kernelPackages = pkgs.linuxPackages_xanmod_latest;
 
     # NTFS support
     supportedFilesystems = [ "ntfs" ];
