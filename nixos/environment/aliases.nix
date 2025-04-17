f111e0a8a9b205b549bf778a2fa723d215bcdaaa
Dawid Weglarz <dawid.weglarz95@gmail.com>
2025-04-17 09:09:30 +0200

    Updated clean alias

--- nixos/environment/aliases.nix
+++ nixos/environment/aliases.nix
@@ -3,7 +3,7 @@
     # NixOS commands
     nix-update = "sudo nix-channel --update && sudo nix flake update --flake /etc/nixos && sudo nixos-rebuild switch --accept-flake-config --flake /etc/nixos && flatpak update -y";
     nix-config = "cd /etc/nixos && vim";
-    nix-full-clean = "sudo nix-store --gc && sudo nix-collect-garbage -d && nix-collect-garbage -d && sudo nixos-rebuild boot";
+    nix-full-clean = "sudo nix-store --gc && sudo nix-collect-garbage -d && nix-collect-garbage -d";
 
     # Aliases
     ai = "aichat";
