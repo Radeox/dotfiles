b41d68e6807db1d2876c5a943540a258002973b8
Dawid Weglarz <dawid.weglarz95@gmail.com>
2025-10-27 09:46:47 +0100

    Remove unused flatpaks

--- nixos/software/flatpak.nix
+++ nixos/software/flatpak.nix
@@ -11,17 +11,13 @@
     # Packages list
     packages = [
       "app.zen_browser.zen"
-      "com.bitwarden.desktop"
       "com.borgbase.Vorta"
       "com.getpostman.Postman"
       "com.github.tchx84.Flatseal"
-      "com.heroicgameslauncher.hgl"
-      "com.jgraph.drawio.desktop"
       "com.mongodb.Compass"
       "com.spotify.Client"
       "com.valvesoftware.Steam"
       "io.ente.photos"
-      "io.github.nozwock.Packet"
       "md.obsidian.Obsidian"
       "org.bleachbit.BleachBit"
       "org.ferdium.Ferdium"
