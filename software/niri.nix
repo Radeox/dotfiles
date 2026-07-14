0bad5b2548a5e47adf47bdf7da09b1d6cca30eba
Dawid Weglarz <dawid.weglarz95@gmail.com>
2026-07-14 09:49:09 +0200

    Use sway instead of cage as wm during login

--- software/niri.nix
+++ software/niri.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, ... }:
+{ pkgs, lib, config, ... }:
 {
   # Enable Niri
   programs.niri.enable = true;
@@ -10,6 +10,33 @@
     theme = {
       package = pkgs.gnome-themes-extra;
       name = "Adwaita-dark";
+    };
+  };
+
+  # Custom greetd session using Sway to mirror displays
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway --unsupported-gpu --config ${pkgs.writeText "greetd-sway-config" ''
+          # Disable borders
+          default_border none
+          default_floating_border none
+
+          # Keyboard layout settings
+          input * {
+            xkb_layout "us,it"
+            xkb_options "caps:escape"
+          }
+
+          # Position all outputs at 0,0 to duplicate/mirror the display space
+          output * pos 0 0
+
+          # Start ReGreet and exit Sway when ReGreet exits (i.e. on login)
+          exec "${config.programs.regreet.package}/bin/regreet; ${pkgs.sway}/bin/swaymsg exit"
+        ''}";
+        user = "greeter";
+      };
     };
   };
 
