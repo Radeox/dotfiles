9450250151f239c1205328c794974596e0b217e6
Dawid Weglarz <dawid.weglarz95@gmail.com>
2026-07-22 19:21:51 +0200

    Change NTFS mount options and driver

--- hosts/monoco.nix
+++ hosts/monoco.nix
@@ -56,13 +56,16 @@
 
   fileSystems."/home/radeox/Vault" = {
     device = "/dev/disk/by-uuid/10042D64042D4E52";
-    fsType = "ntfs";
+    fsType = "ntfs3";
     options = [
       "uid=1000"
       "gid=100"
-      "dmask=022"
-      "fmask=113"
-      "umask=0022"
+      "rw"
+      "user"
+      "exec"
+      "umask=000"
+      "nofail"
+      "iocharset=utf8"
     ];
   };
 
