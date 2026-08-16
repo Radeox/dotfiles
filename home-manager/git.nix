f82c7f7d610f038ace57d2c7cd7cd49843567000
Dawid Weglarz <dawid.weglarz95@gmail.com>
2026-08-16 12:38:22 +0200

    Add safe dir to git config

--- home-manager/git.nix
+++ home-manager/git.nix
@@ -3,6 +3,11 @@
   # Git configuration
   programs.git = {
     enable = true;
+
+    extraConfig = {
+      safe.directory = "/etc/nixos";
+    };
+
     settings.user = {
       name = "Dawid Weglarz";
       email = "dawid.weglarz95@gmail.com";
